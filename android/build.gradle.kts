e817041fa3b81f706bffc676cfb199cff5f32ef8
Lovepreet Singh <2421729.it.cec@cgc.edu.in>
2026-03-25 16:34:14 +0000

    final apk workflow

---
+++ android/build.gradle.kts
@@ -0,0 +1,34 @@
+import org.gradle.api.tasks.Delete
+import org.gradle.api.file.Directory
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+// ✅ Fix build directory (Flutter optimization)
+val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
+rootProject.layout.buildDirectory.set(newBuildDir)
+
+subprojects {
+    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
+    project.layout.buildDirectory.set(newSubprojectBuildDir)
+}
+
+subprojects {
+    project.evaluationDependsOn(":app")
+}
+
+// ✅ Clean task
+tasks.register<Delete>("clean") {
+    delete(rootProject.layout.buildDirectory)
+}
